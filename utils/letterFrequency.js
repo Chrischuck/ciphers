efcb1ba0fa163a87f0d130be10115e0a33dcabd3
Chris <chrischuck35@gmail.com>
2018-02-04 11:28:18 -0800

    adds a frequency counter

---
+++ utils/letterFrequency.js
@@ -0,0 +1,27 @@
+module.exports = letterFrequency
+
+function letterFrequency(text) {
+  const frequencyMap = {}
+  // turn text into array and remove spaces
+  const splitText = text
+    .split('')
+    .filter(letter => letter !== ' ')
+
+  splitText.forEach(letter => {
+    if (!frequencyMap[letter]) {
+      frequencyMap[letter] = 1
+    } else {
+      frequencyMap[letter] += 1
+    }
+  })
+
+  const mapKeys = Object.keys(frequencyMap)
+  // turn into precentage
+  mapKeys.forEach(key => {
+    frequencyMap[key] = frequencyMap[key] / splitText.length
+  })
+
+  return frequencyMap
+}
+
+console.log(letterFrequency('hello'))
