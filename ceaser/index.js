ac50c60c6ece3eba5dd6605968dc96924ea03f5d
Chris <chrischuck35@gmail.com>
2018-02-04 00:23:39 -0800

    Ceaser cipher

---
+++ ceaser/index.js
@@ -0,0 +1,39 @@
+const textToNumbers = require('./utils/textToNumbers.js')
+
+const plainText = 'fourscoreandsevenyearsago'
+
+const shift = 3
+
+// encrypt the plaintext
+const split = plainText.split('')
+
+const numericalText = split.map(letter => textToNumbers[letter])
+
+const numericalCipher = numericalText.map(number => (number + shift) % 27)
+
+const keys = Object.keys(textToNumbers)
+
+const cipherTextArray = numericalCipher.map(number => keys[number])
+
+let cipherText = ''
+
+cipherTextArray.forEach(letter => {
+  cipherText += letter
+})
+
+// decripyt the ceaser cipher
+const reverseNumericalCipher = numericalCipher.map(number => (number + 27 - shift) % 27)
+const reverseCipherTextArray = reverseNumericalCipher.map(number => keys[number])
+
+let decipheredText = ''
+
+reverseCipherTextArray.forEach(letter => {
+  decipheredText += letter
+})
+
+// console logging
+console.log(plainText)
+console.log('\n')
+console.log(cipherText)
+console.log('\n')
+console.log(decipheredText)
