524fb7bce73d30c7a2fc57958ddff9b643b39610
Chris <chrischuck35@gmail.com>
2018-02-05 15:52:03 -0800

    Fixes counting digraphs and trigraphs

--- utils/countDigraphs.js
+++ utils/countDigraphs.js
@@ -2,16 +2,14 @@ module.exports = countDigraphs
 
 function countDigraphs(text) {
   const digraphs = {}
-  let textCopy = text
 
-  for (let i = 0; i < textCopy.length; i++) {
-    const firstTwo = textCopy.slice(0, 2)
+  for (let i = 0; i < text.length; i++) {
+    const firstTwo = text.slice(i, i + 2)
     if (!digraphs[firstTwo]) {
       digraphs[firstTwo] = 1
     } else {
       digraphs[firstTwo] += 1
     }
-    textCopy = textCopy.slice(1)
   }
 
   const keys = Object.keys(digraphs)
@@ -21,7 +19,7 @@ function countDigraphs(text) {
       frequency: digraphs[key]
     }
   })
-  .filter(digraph => digraph.frequency > 1) // should we filter out trigraphs of 1?
+  .filter(digraph => digraph.frequency > 1 && digraph.digraph.length === 2) // should we filter if less than 1
   .sort((a, b) => b.frequency - a.frequency)
 
   return digraphArray
--- utils/countTrigraphs.js
+++ utils/countTrigraphs.js
@@ -2,16 +2,14 @@ module.exports = countTrigraphs
 
 function countTrigraphs(text) {
   const trigraphs = {}
-  let textCopy = text
 
-  for (let i = 0; i < textCopy.length; i++) {
-    const firstThree = textCopy.slice(0, 3)
+  for (let i = 0; i < text.length; i++) {
+    const firstThree = text.slice(i, i + 3)
     if (!trigraphs[firstThree]) {
       trigraphs[firstThree] = 1
     } else {
       trigraphs[firstThree] += 1
     }
-    textCopy = textCopy.slice(1)
   }
 
   const keys = Object.keys(trigraphs)
@@ -21,7 +19,7 @@ function countTrigraphs(text) {
       frequency: trigraphs[key]
     }
   })
-  .filter(trigraph => trigraph.frequency > 1)
+  .filter(trigraph => trigraph.frequency > 1 && trigraph.trigraph.length === 3)
   .sort((a, b) => b.frequency - a.frequency)
 
   return trigraphArray
