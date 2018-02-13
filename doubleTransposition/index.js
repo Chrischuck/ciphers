99010d4eeac9d1d9d02d56d5be4a4de6ed97e866
Chris <chrischuck35@gmail.com>
2018-02-12 16:16:48 -0800

    Starts decrpytion for double transposition

--- doubleTransposition/index.js
+++ doubleTransposition/index.js
@@ -1,10 +1,22 @@
 const rowLength = 3
 const colLength = 5 
 
+const transpositions = {
+  rows: [
+    [0, 2],
+    [4, 1]
+  ],
+  columns: [
+    [1, 2]
+  ]
+}
+
 const plainText = 'attack at dawn'
 
-function encrypt(plainText) {
+function encrypt(plainText, transpositions, rowLength, colLength) {
   const matrix = []
+  const { rows, columns } = transpositions
+
   for (let i = 0; i < colLength; i++) {
     matrix.push([])
   }
@@ -23,15 +35,6 @@ function encrypt(plainText) {
     }
   }
 
-  // we will switch rows 1 and 3, 5 and 2
-  // we will switch col 2 and 3
-  const rows = [
-    [0, 2],
-    [4, 1]
-  ]
-  const columns = [
-    [1, 2]
-  ]
   rows.forEach(swap => {
     const temp = [ ...matrix[ swap[0] ] ]
     matrix[ swap[0] ] = matrix[ swap[1] ]
@@ -55,8 +58,53 @@ function encrypt(plainText) {
   return ciperText
 }
 
-function decrypt() {
-  console.log(decrpyt)
+
+
+function decrypt(cipherText, transpositions, rowLength, colLength) {
+  const matrix = []
+  const { rows, columns } = transpositions
+  // reverse rows and columns
+  
+  for (let i = 0; i < colLength; i++) {
+    matrix.push([])
+  }
+  const split = cipherText.split('')
+  let splitCounter = 0;
+
+  for (let i = 0; i < colLength; i++) {
+    for (let j = 0; j < rowLength; j++) {
+      const letter = split[splitCounter]
+      if (letter) {
+        matrix[i][j] = letter
+      } else {
+        matrix[i][j] = ' '
+      }
+      splitCounter += 1
+    }
+  }
+
+  rows.forEach(swap => {
+    const temp = [ ...matrix[ swap[0] ] ]
+    matrix[ swap[0] ] = matrix[ swap[1] ]
+    matrix[ swap[1] ] = temp
+  })
+
+  columns.forEach(swap => {
+    for (let i = 0; i < colLength; i++) {
+      const temp = matrix[i][ swap[0] ]
+      matrix[i][ swap[0] ] = matrix[i][ swap[1] ]
+      matrix[i][ swap[1] ] = temp
+    }
+  })
+
+  let plainText = ''
+  matrix.forEach(row => {
+    row.forEach(letter => {
+      plainText += letter
+    })
+  })
+  return plainText
 }
 
-console.log(encrypt(plainText))
+const cipherText = encrypt(plainText, transpositions, rowLength, colLength)
+const plainText = decrypt(cipherText, transpositions, rowLength, colLength)
