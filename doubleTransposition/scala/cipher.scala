959d0b01317d1c0f5e73cd0a0509bacc47206edf
Chris <chrischuck35@gmail.com>
2018-03-03 23:00:59 -0800

    Starts scala double transposition cipher

---
+++ doubleTransposition/scala/cipher.scala
@@ -0,0 +1,17 @@
+ import scala.collection.mutable.{Map}
+
+class DoubleTransposition(val r: Int, val c: Int, val k: Map[String, Array[Array[Int]]]) {
+  var rows: Int = r
+  var cols: Int = c
+  var key: Map[String, Array[Array[Int]]] = k
+
+  def encrypt(plaintext: String) {
+
+  }
+}
+
+object Run {
+  def main(agrs: Array[String]) {
+    println("Hello, world!")
+  }
+}
